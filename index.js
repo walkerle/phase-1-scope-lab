76cfd1213f4d1ff39c5032de6cdbfbdb809dd73c
Walker Le <walker.le88@gmail.com>
2022-12-27 17:52:02 -0700

    commit tree d28e06423a56290b432bf7929c1317f82afef3ea

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some name';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new name'
+}
+
+// console.log(customerName);
+// upperCaseCustomerName();
+// console.log(customerName);
+// setBestCustomer();
+// console.log(bestCustomer);
+// overwriteBestCustomer();
+// console.log(bestCustomer)
+// console.log(leastFavoriteCustomer);
+// changeLeastFavoriteCustomer();
+// console.log(leastFavoriteCustomer);
